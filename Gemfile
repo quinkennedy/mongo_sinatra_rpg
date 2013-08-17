a121bc8ad47dad296d1b08e9dba7a2073ddac391
Quin Kennedy <quinkennedy@gmail.com>
2013-08-17 18:53:55 -0400

    got my head a little bit around mongo and sinatra

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 gem 'sinatra', '1.1.0'
 gem 'thin'
+gem 'mongo'
+gem 'bson_ext'
--- web.rb
+++ web.rb
@@ -1,5 +1,27 @@
 require 'sinatra'
+require 'mongo'
+require 'uri'
+
+def get_connection
+	return @db_collection if @db_collection
+	db = URI.parse(ENV['MONGOHQ_URL'])
+	db_name = db.path.gsub(/^\//, '')
+	db_connection = Mongo::MongoClient.new(db.host, db.port).db(db_name)
+	db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.password.nil?)
+	@db_collection = db_connection.collection("rpg_exercise")
+	return @db_collection
+end
 
 get '/' do
-  "Hello, world"
+	#{}"Hello, world"
+	#String.try_convert(ENV["MONGOHQ_URL"])
+	String.try_convert(get_connection)
+end
+
+get '/:quiz' do
+	"you are at #{params[:quiz]}"
+end
+
+get '/:quiz/:question' do
+	"you are at #{params[:quiz]} in #{params[:question]}"
 end
